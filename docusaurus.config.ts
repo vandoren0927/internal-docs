69a63eea1ffb5f20278163bdb0b9f16524c5805e
vandoren0927 <vandorenwong@gmail.com>
2025-05-30 03:19:44 +0800

    new docusaurus.config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
       links: [
         {
           title: 'Docs',
-          items: [{ label: 'Tutorial', to: '/kb/' }],
+          items: [{ label: 'Tutorial', to: '/kb/index' }],
         },
         {
           title: 'Community',
